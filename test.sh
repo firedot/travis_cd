ee97787170124c6f1407437a5fb15c8d2da609bd
FireWorm <linked2u@gmail.com>
2018-11-06 13:08:07 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+ 
+ # The following line creates a variable in which the output from the execution of ```hello.sh``` is stored
  output=$(bash hello.sh)
+ 
+ # if statement that tests for particular output the contents of the variable that we created before
  if [ "${output}" == "hello" ]; then
   echo 'GOOD: Test pass'
 else
